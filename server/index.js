ea2ad96f5c3dd07f12e2ef666367d1c995b18c6b
robdeer1 <r_deer@fanshaweonline.ca>
2024-11-25 08:46:42 -0500

    changed index.js file

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
 
-app.use(express.static(path.join(__dirname, "client", "dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
