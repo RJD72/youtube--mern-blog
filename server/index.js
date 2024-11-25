ced6210c7ee8194459f5cbcb26185e49f065d2a5
robdeer1 <r_deer@fanshaweonline.ca>
2024-11-25 08:09:24 -0500

    removed the explicit use of the __dirname

--- server/index.js
+++ server/index.js
@@ -9,8 +9,6 @@ const authRouter = require("./routes/authRoutes");
 const postRouter = require("./routes/postRoute");
 const commentRouter = require("./routes/commentRoute");
 
-const __dirname = path.resolve();
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -23,7 +21,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
